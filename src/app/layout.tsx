cccee97db99f39e857788598bcdee010496682aa
Asif Mahamud Shaon <asifmahamud669@gmail.com>
2026-02-08 17:34:31 +0600

    Add project files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,30 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Asif Mahamud Shaon | System Engineer",
+    title: "Asif Mahamud Shaon | Full Stack Engineer",
     description: "Portfolio of Asif Mahamud Shaon, a Full-Stack Software Engineer focused on scalable digital systems.",
+    openGraph: {
+        title: "Asif Mahamud Shaon | Full Stack Engineer",
+        description: "Portfolio of Asif Mahamud Shaon, a Full-Stack Software Engineer focused on scalable digital systems.",
+        url: "https://asifmahamudshaon.vercel.app",
+        siteName: "Asif Mahamud Shaon Portfolio",
+        images: [
+            {
+                url: "/images/mediwise.png", // Default image
+                width: 1200,
+                height: 630,
+                alt: "Asif Mahamud Shaon Portfolio",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Asif Mahamud Shaon | Full Stack Engineer",
+        description: "Portfolio of Asif Mahamud Shaon, a Full-Stack Software Engineer focused on scalable digital systems.",
+        images: ["/images/mediwise.png"], // Default image
+    },
 };
 
 export default function RootLayout({
